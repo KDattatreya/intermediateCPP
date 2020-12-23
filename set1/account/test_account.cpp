cc7689efd4170358263158f3477060d429dfe6d3
stepin104474 <69111724+KDattatreya@users.noreply.github.com>
2020-12-23 16:28:13 +0530

    Update test_account.cpp

--- set1/account/test_account.cpp
+++ set1/account/test_account.cpp
@@ -7,34 +7,34 @@ TEST(Account, DefaultConstructor) {
 }
 
 TEST(Account, ParameterizedConstructor) {
-  Account a1("1001", "ABC", 5000.0);
-  EXPECT_EQ(5000.0, a1.getBalance());
+  Account a1("001", "ABC", 1000.0);
+  EXPECT_EQ(1000.0, a1.getBalance());
 }
 
 TEST(Account, CopyConstructor) {
-  Account a1("1001", "ABC", 5000.0);
+  Account a1("001", "ABC", 1000.0);
   Account a2(a1);
-  EXPECT_EQ(5000.0, a2.getBalance());
+  EXPECT_EQ(1000.0, a2.getBalance());
 }
 
 TEST(Account, Credit) {
-  Account a1("1001", "ABC", 5000.0);
-  a1.credit(3000);
-  EXPECT_EQ(8000.0, a1.getBalance());
+  Account a1("001", "ABC", 1000.0);
+  a1.credit(2000);
+  EXPECT_EQ(3000.0, a1.getBalance());
 }
 
 TEST(Account, Debit) {
-  Account a1("1001", "ABC", 5000.0);
-  a1.debit(1200);
-  EXPECT_EQ(3800.0, a1.getBalance());
+  Account a1("001", "ABC", 1000.0);
+  a1.debit(500);
+  EXPECT_EQ(500.0, a1.getBalance());
 }
 
 TEST(Account, Transaction) {
-  Account a1("1001", "ABC", 5000.0);
-  a1.debit(1200);
-  a1.credit(3400);
-  a1.credit(3000);
-  EXPECT_EQ(10200.0, a1.getBalance());
+  Account a1("001", "ABC", 1000.0);
+  a1.debit(500);
+  a1.credit(600);
+  a1.credit(200);
+  EXPECT_EQ(1300.0, a1.getBalance());
 }
 
 int main() {
